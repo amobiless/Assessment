4d7b57e601e7d546ca230fee5a35e1d275b3335f
amobiless <barrella@middleton.school.nz>
2021-08-05 14:42:36 +1200

    Revert "Create 03_splitter_function_v1.py"
    This reverts commit 36230b445115964cb37af8c57bed9b3cb3738675.

--- 03_splitter_function_v1.py
+++ 03_splitter_function_v1.py
@@ -1,23 +1,16 @@
-"""Initial version of an item splitter which splits the participant's name and time
-Version 1 - this version tells the user if the regex follows the written formula
-Allows both . and : symbols for time.
-Created by Ashlee Barrell
-29/07/2021
-"""
+result_list = [
+    "jay 4.30",
+    "harry 4:30"
+]
 
-import re  # This is the Regular Expression module
+for participant in result_list:
+    participant = participant.split()
+    name = participant[0].title()
+    time = participant[1]
+    if name.isspace() or name.isdigit():
+        print("Please enter a valid name")
+    if time.isdigit():
+        participant.remove(participant[1])
+    product = " ".join([str(word) for word in participant])
 
-# Line has both name and time in it and will be split so it it name and time
-name_and_time = "jay 5:30"  # Change to input statement in due course
-
-
-# The regex format below is expecting: number <space> number
-mixed_regex = "[a-z]{1,}\s\d{1,3}[.:]\d{1,2}"
-# \d for a digit, /d{1,3] allows 1-3 digits, /s for space, [a-z] for letters, {1,} means at least one letter,
-# but can be limited
-
-# Testing to see if the recipe line matches the regular expression
-if re.match(mixed_regex, name_and_time):
-    print("True")
-else:
-    print("False")
+    print(f"Name: = {name}, Time = {time} seconds")
