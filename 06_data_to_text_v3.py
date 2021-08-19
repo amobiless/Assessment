69afb5ca8928491888bac8a89c50bb81f6101f57
amobiless <barrella@middleton.school.nz>
2021-08-19 23:00:37 +1200

    Create 06_data_to_text_v3.py

---
+++ 06_data_to_text_v3.py
@@ -0,0 +1,27 @@
+import re  # Importing Regular Expressions
+
+# Data to be outputted
+result_list = [['Jay', 3.7], ['Maebh', 4.0], ['Luna', 4.3]]
+result_list.sort()
+for element in result_list:
+    print(f"{(result_list.index(element)+1)}. Name: {element[0]}\t Time: {element[1]}")
+
+# Get file name which can't be blank or invalid
+# Assume valid data
+filename = input("Please enter a file name: ")
+
+valid_file = "[A-Zaz]"
+if re.match(valid_file, filename):
+
+    # Add .txt suffix
+    filename = filename + ".txt"
+
+    # Creates file to hold data
+    f = open(filename, "w+")
+
+    for element in result_list:
+        f.write("100m Race")
+        f.write(f"{(result_list.index(element))} Name: {element[0]} - Time:{element[1]} seconds\n")
+
+    # Close file
+    f.close()
